7250865d9a0460c086ef8590bbe0cf43fc6b2ee7
jinlee4140 <jinlee4140@gmail.com>
2015-08-27 22:54:31 -0400

    missing colon

--- test_calculator.py
+++ test_calculator.py
@@ -13,12 +13,17 @@ class TddCalculator(unittest.TestCase):
 	def test_calculator_returns_error_message_if_both_args_not_numbers(self):
 		self.assertRaises(ValueError, self.calc.add, 'two', 'three')
 
-	# def test_calculator_returns_error_message_second(self):
-	# 	self.assertRaises(ValueError, self.calc.add, 2 , 'three')
+	def test_calculator_returns_error_message_second(self):
+		self.assertRaises(ValueError, self.calc.add, 2 , 'three')
+
+
+	def test_calculator_assert_not_equal(self):
+		result = self.calc.add(3, 4)
+		self.assertNotEqual(6, result)
 
 	
 
  
  
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
