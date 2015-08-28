0489b3584c790807cbae5adcd6e29647cacc3c8f
jinlee4140 <jinlee4140@gmail.com>
2015-08-27 22:38:57 -0400

    more tdd

--- test_calculator.py
+++ test_calculator.py
@@ -2,11 +2,16 @@ import unittest
 from calculator import Calculator
  
 class TddCalculator(unittest.TestCase):
- 
+
+	def setUp(self):
+		self.calc = Calculator()
+        
     def test_calculator(self):
-        calc = Calculator()
-        result = calc.add(2,2)
+        result = self.calc.add(2,2)
         self.assertEqual(4, result)
+
+    def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+    	self.assertRaises(ValueError, self.calc.add, 'two', 'three')
  
  
 # if __name__ == '__main__':
