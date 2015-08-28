c960dfd13675a0a59af4f2825ff384a59e126113
jinlee4140 <jinlee4140@gmail.com>
2015-08-27 22:50:29 -0400

    modification on calculation.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,9 @@
 class Calculator(object):
  
     def add(self, x, y):
-        return x+y
+        number_types = (int, long, float, complex)
+
+        if isinstance(x, number_types) and isinstance(y, number_types):
+        	return x + y
+        else
+        	raise ValueError
--- test_calculator.py
+++ test_calculator.py
@@ -5,13 +5,19 @@ class TddCalculator(unittest.TestCase):
 
 	def setUp(self):
 		self.calc = Calculator()
-        
-    def test_calculator(self):
-        result = self.calc.add(2,2)
-        self.assertEqual(4, result)
+				
+	def test_calculator_add_method_returns_correct_result(self):
+		result = self.calc.add(2, 2)
+		self.assertEqual(4, result)
 
-    def test_calculator_returns_error_message_if_both_args_not_numbers(self):
-    	self.assertRaises(ValueError, self.calc.add, 'two', 'three')
+	def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+		self.assertRaises(ValueError, self.calc.add, 'two', 'three')
+
+	# def test_calculator_returns_error_message_second(self):
+	# 	self.assertRaises(ValueError, self.calc.add, 2 , 'three')
+
+	
+
  
  
 # if __name__ == '__main__':
